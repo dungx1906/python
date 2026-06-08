641e52fdcbea17fe29565cfcf8b59ed9dbfe429a
dungx1906 <conanshinan@gmail.com>
2026-06-08 20:13:52 +0700

    ss12-13

--- SS12/TH1.py
+++ SS12/TH1.py
@@ -1,52 +1,137 @@
 cart_items = [
-         {
-         	"id": "P001",
-         	"name": "Dien thoai iPhone 15",
-         	"number": 1,
-         	"price": 25000000
-         },
-         {
-         	"id": "P002",
-         	"name": "Op lung Silicon",
-         	"number": 2,
-         	"price": 150000
-         }
+    {
+        "id": "P001",
+        "name": "Dien thoai iPhone 15",
+        "number": 1,
+        "price": 25000000
+    },
+    {
+        "id": "P002",
+        "name": "Op lung Silicon",
+        "number": 2,
+        "price": 150000
+    }
 ]
 
-# In tiêu đề
-print("--- CHI TIẾT GIỎ HÀNG ---")
+while True:
+    print("\n===== AMAZON CART MANAGEMENT =====")
+    print("1. Xem giỏ hàng và tổng tiền")
+    print("2. Thêm sản phẩm / Tăng số lượng")
+    print("3. Cập nhật số lượng sản phẩm")
+    print("4. Xóa sản phẩm")
+    print("5. Thoát")
 
-# Định nghĩa Header và căn lề (< là căn trái, số là độ rộng của cột)
-header = f"{'STT':<3} | {'Mã SP':<6} | {'Tên Sản Phẩm':<22} | {'SL':<2} | {'Đơn Giá':<12} | {'Thành Tiền':<12}"
-print(header)
+    choice = input("Nhập lựa chọn: ")
 
-# In đường kẻ ngang (độ dài bằng đúng độ dài của header)
-print("-" * len(header))
+    if not choice.isdigit():
+        print("Lựa chọn không hợp lệ!")
+        continue
 
-total_product = 0
-total_money = 0
+    choice = int(choice)
 
-# Duyệt qua từng sản phẩm và in ra
-for i, sp in enumerate(cart_items, start = 1):
-    thanh_tien = sp["number"] * sp["price"]
+    # Chức năng 1
+    if choice == 1:
+        total_quantity = 0
+        total_money = 0
 
-    # Định dạng từng dòng với độ rộng cột tương ứng với header
-    row = (
-        f"{i:<3} | "
-        f"{sp["id"]:<6} | "
-        f"{sp["name"]:<22} | "
-        f"{sp["number"]:<2} | "
-        f"{(sp["price"]):<12} | "
-        f"{(thanh_tien):<12}"
-    )
+        print("\nID\tTên sản phẩm\t\tSL\tĐơn giá")
 
-    total_product += sp["number"]
-    total_money += thanh_tien
+        for item in cart_items:
+            print(item["id"], "\t",
+                  item["name"], "\t",
+                  item["number"], "\t",
+                  item["price"])
 
-    print(row)
+            total_quantity += item["number"]
+            total_money += item["number"] * item["price"]
 
-# In đường kẻ ngang chốt lại bảng
-print("-" * len(header))
+        print("Tổng số lượng:", total_quantity)
+        print("Tổng tiền:", total_money, "VNĐ")
 
-print(f"Tổng số lượng sản phẩm trong giỏ: {total_product}")
-print(f"TỔNG TIỀN THANH TOÁN {total_money:,}đ")
+    # Chức năng 2
+    elif choice == 2:
+        product_id = input("Nhập mã sản phẩm: ")
+        product_name = input("Nhập tên sản phẩm: ")
+
+        quantity = input("Nhập số lượng: ")
+        price = input("Nhập đơn giá: ")
+
+        if not quantity.isdigit() or not price.isdigit():
+            print("Số lượng và đơn giá phải là số!")
+            continue
+
+        quantity = int(quantity)
+        price = int(price)
+
+        if quantity <= 0 or price < 0:
+            print("Dữ liệu không hợp lệ!")
+            continue
+
+        found = False
+
+        for item in cart_items:
+            if item["id"] == product_id:
+                item["number"] += quantity
+                found = True
+                print("Đã tăng số lượng sản phẩm.")
+                break
+
+        if found == False:
+            cart_items.append({
+                "id": product_id,
+                "name": product_name,
+                "number": quantity,
+                "price": price
+            })
+            print("Thêm sản phẩm thành công!")
+
+    # Chức năng 3
+    elif choice == 3:
+        product_id = input("Nhập mã sản phẩm: ")
+        new_quantity = input("Nhập số lượng mới: ")
+
+        if not new_quantity.isdigit():
+            print("Số lượng phải là số!")
+            continue
+
+        new_quantity = int(new_quantity)
+
+        if new_quantity <= 0:
+            print("Số lượng phải lớn hơn 0!")
+            continue
+
+        found = False
+
+        for item in cart_items:
+            if item["id"] == product_id:
+                item["number"] = new_quantity
+                found = True
+                print("Cập nhật thành công!")
+                break
+
+        if found == False:
+            print("Mã sản phẩm không tồn tại trong giỏ hàng.")
+
+    # Chức năng 4
+    elif choice == 4:
+        product_id = input("Nhập mã sản phẩm cần xóa: ")
+
+        found = False
+
+        for item in cart_items:
+            if item["id"] == product_id:
+                cart_items.remove(item)
+                found = True
+                print("Xóa sản phẩm thành công!")
+                break
+
+        if found == False:
+            print("Mã sản phẩm không tồn tại trong giỏ hàng.")
+
+    # Chức năng 5
+    elif choice == 5:
+        print("Thoát chương trình!")
+        break
+
+    else:
+        print("Lựa chọn phải từ 1 đến 5.")
---
+++ SS13/mini-project.py
@@ -0,0 +1,210 @@
+parking_lot = []
+next_id = 1
+
+while True:
+
+    print("\n========== SMART PARKING SYSTEM ==========")
+    print("1. Check-in xe")
+    print("2. Báo cáo tồn kho")
+    print("3. Tìm kiếm xe")
+    print("4. Check-out xe")
+    print("5. Thoát")
+
+    choice = input("Chọn chức năng: ")
+
+    if not choice.isdigit():
+        print("ERR-01: Lựa chọn không hợp lệ!")
+        continue
+
+
+    if choice == "1":
+
+        plate = input("Nhập biển số xe: ").strip()
+
+        if plate == "":
+            print("ERR-02: Biển số không được để trống!")
+            continue
+
+        duplicate = False
+
+        for vehicle in parking_lot:
+            if vehicle["plate"] == plate:
+                duplicate = True
+                break
+
+        if duplicate:
+            print("ERR-03: Biển số đã tồn tại trong bãi!")
+            continue
+
+        while True:
+
+            vehicle_type = input(
+                "Loại xe (1-Xe máy, 2-Ô tô): "
+            )
+
+            if vehicle_type.isdigit():
+
+                vehicle_type = int(vehicle_type)
+
+                if vehicle_type == 1:
+                    vehicle_type = "Xe máy"
+                    break
+
+                elif vehicle_type == 2:
+                    vehicle_type = "Ô tô"
+                    break
+
+            print("ERR-01: Loại xe không hợp lệ!")
+
+        while True:
+
+            entry_time = input(
+                "Nhập giờ vào (0-23): "
+            )
+
+            if entry_time.isdigit():
+                entry_time = int(entry_time)
+
+                if 0 <= entry_time <= 23:
+                    break
+
+            print("ERR-01: Giờ vào không hợp lệ!")
+
+        parking_lot.append(
+            {
+                "id": next_id,
+                "plate": plate,
+                "type": vehicle_type,
+                "entry_time": entry_time
+            }
+        )
+
+        print("Check-in thành công!")
+
+        next_id += 1
+
+    elif choice == "2":
+
+        if len(parking_lot) == 0:
+            print("ERR-02: Bãi xe hiện đang trống!")
+        else:
+
+            print("\nDANH SÁCH XE TRONG BÃI")
+
+            print(
+                f"{'ID':<6}"
+                f"{'Biển số':<15}"
+                f"{'Loại xe':<15}"
+                f"{'Giờ vào':<10}"
+            )
+
+            print("-" * 50)
+
+            for vehicle in parking_lot:
+
+                print(
+                    f"{vehicle['id']:<6}"
+                    f"{vehicle['plate']:<15}"
+                    f"{vehicle['type']:<15}"
+                    f"{vehicle['entry_time']:<10}"
+                )
+
+    elif choice == "3":
+
+        plate = input(
+            "Nhập biển số cần tìm: "
+        ).strip()
+
+        if plate == "":
+            print("ERR-02: Biển số không được để trống!")
+            continue
+
+        found = False
+
+        for vehicle in parking_lot:
+
+            if vehicle["plate"] == plate:
+
+                found = True
+
+                print("\nThông tin xe:")
+
+                print(vehicle)
+
+                break
+
+        if not found:
+            print("ERR-04: Không tìm thấy xe!")
+
+    elif choice == "4":
+
+        plate = input(
+            "Nhập biển số cần check-out: "
+        ).strip()
+
+        if plate == "":
+            print("ERR-02: Biển số không được để trống!")
+            continue
+
+        found = False
+
+        for vehicle in parking_lot:
+
+            if vehicle["plate"] == plate:
+
+                found = True
+
+                while True:
+
+                    exit_time = input(
+                        "Nhập giờ ra (0-23): "
+                    )
+
+                    if exit_time.isdigit():
+
+                        exit_time = int(exit_time)
+
+                        if 0 <= exit_time <= 23:
+
+                            if exit_time >= vehicle["entry_time"]:
+                                break
+
+                            else:
+                                print(
+                                    "ERR-05: Giờ ra phải lớn hơn hoặc bằng giờ vào!"
+                                )
+                                continue
+
+                    print("ERR-01: Giờ ra không hợp lệ!")
+
+                parking_hours = (
+                    exit_time - vehicle["entry_time"]
+                )
+
+                if vehicle["type"] == "Xe máy":
+                    fee = parking_hours * 5000
+                else:
+                    fee = parking_hours * 20000
+
+                print("\n===== HÓA ĐƠN =====")
+                print("Biển số:", vehicle["plate"])
+                print("Loại xe:", vehicle["type"])
+                print("Số giờ gửi:", parking_hours)
+                print("Phí gửi xe:", fee, "VNĐ")
+
+                parking_lot.remove(vehicle)
+
+                print("Check-out thành công!")
+
+                break
+
+        if not found:
+            print("ERR-04: Không tìm thấy xe!")
+
+    elif choice == "5":
+
+        print("Đã thoát chương trình!")
+        break
+
+    else:
+        print("ERR-01: Lựa chọn không hợp lệ!")
